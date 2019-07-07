9ec814513d34a6f118e727f2258e32a2b486b9da
Erik Kimsey <kimseyerik@gmail.com>
2019-07-07 11:49:29 -0400

    Build Menu component

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * sub-item feeds into collapse-item
+ * collapse-item feeds into menu-container
+ * in index.js, querySelector('menu-container'), then...
+ * ...append menu-container to body
+ */
