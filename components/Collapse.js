ea6fc985e909c49f669241ce6d0d31543144259a
Erik Kimsey <kimseyerik@gmail.com>
2019-07-08 10:36:53 -0400

    A rudimentary collapse is working

--- components/Collapse.js
+++ components/Collapse.js
@@ -19,10 +19,27 @@ template.innerHTML = `
     .tab {
     border: none;
     cursor: pointer;
+    width:100px;
+    height:33px;
+    background-color:pink;
     }
 
+    .content {
+      display:flex;
+      flex-flow:column;
+      color:white;
+      padding: 0 18px;
+      height:0;
+      overflow: hidden;
+      background-color: #f1f1f1;
+      transition: all 0.2s ease-out;
+    }
+    a {
+      color:white;
+    }
     .active, .collapsible:hover {
       background-color: #555;
+      height:auto;
     }
 
     ul {
@@ -30,17 +47,14 @@ template.innerHTML = `
       padding: 0;
     }
     
-    .content {
-      padding: 0 18px;
-      max-height: 0;
-      overflow: hidden;
-      transition: max-height 0.2s ease-out;
-      background-color: #f1f1f1;
-    }
 </style>
 <div class="tab">Tab</div>
+<div class="content">
+    <a>DKFJSKDJF</a>
+    <a>DKFJSKDJF</a>
+    <a>DKFJSKDJF</a>
+</div>
 
-<div class="items">YOYOYO</div>
 `;
 
 class Collapsible extends HTMLElement {
@@ -50,8 +64,17 @@ class Collapsible extends HTMLElement {
 		this._shadowRoot.appendChild(template.content.cloneNode(true));
 		console.log(this._shadowRoot);
 
-		this.$subItemList = this._shadowRoot.querySelector('items');
-		this.$tab = this._shadowRoot.querySelector('tab');
+		this.$subItemList = this._shadowRoot.querySelector('.content');
+		console.log(this.$subItemList);
+
+		this.$tab = this._shadowRoot.querySelector('.tab');
+		this.$tab.addEventListener('click', this._toggleCollapse.bind(this));
+	}
+
+	_toggleCollapse() {
+		console.log(this.$subItemList.classList);
+		// if(this.$subItemList.classList)
+		this.$subItemList.classList.toggle('active');
 	}
 
 	connectedCallback() {
@@ -72,3 +95,19 @@ class Collapsible extends HTMLElement {
 }
 
 window.customElements.define('collapsible-list', Collapsible);
+
+/**
+ * <div class="content">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <div><a href="https://erikkimsey.com">CLICKEROO</a></div>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+ */
